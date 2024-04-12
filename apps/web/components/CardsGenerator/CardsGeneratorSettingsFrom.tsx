0def59e71119b7f6dd2db82acca47cc250a50803
ryinner <vinograd18062002@gmail.com>
2024-04-12 15:50:45 +0500

    start generation

---
+++ apps/web/components/CardsGenerator/CardsGeneratorSettingsFrom.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import type React from 'react';
+
+export default function CardsGeneratorSettingsFrom(): React.ReactNode {
+  return <form>
+
+  </form>
+}
--- apps/web/locales/messages/en.ts
+++ apps/web/locales/messages/en.ts
@@ -1,5 +1,9 @@
 export default {
   app: {
     name: "TTRPG Helper",
+    description: "The Best TTRPG Helper in the world"
+  },
+  nav: {
+    cardsGenerator: "Cards Generator",
   },
 } as const;
