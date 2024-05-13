6c1922974527009dd83938de503bb97bd3170a8e
ryinner <vinograd18062002@gmail.com>
2024-05-13 09:11:50 +0500

    styles

--- packages/helpers/src/object.ts
+++ packages/helpers/src/object.ts
@@ -1,5 +1,3 @@
-export function jsonCopy <T extends object>({ object }: {
-  object: T
-}): T {
+export function jsonCopy<T extends object>({ object }: { object: T }): T {
   return JSON.parse(JSON.stringify(object));
 }
