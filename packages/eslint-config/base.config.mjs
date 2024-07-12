3b0f0b81cfd98dec8946034420a6d83d10f40591
ryinner <vinograd18062002@gmail.com>
2024-07-12 08:30:37 +0500

    eslint package config

---
+++ packages/eslint-config/base.config.mjs
@@ -0,0 +1,49 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginPromise from 'eslint-plugin-promise';
+import pluginStylisticTs from '@stylistic/eslint-plugin-ts';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {
+    ignores: ['.node_modules/*', '.dist/*', '*.mjs']
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  pluginPromise.configs['flat/recommended'],
+  pluginStylisticTs.configs['all-flat'],
+  {
+    rules: {
+      '@typescript-eslint/consistent-type-assertions': ['error', { assertionStyle: 'angle-bracket' }],
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', fixStyle: 'inline-type-imports' }],
+      '@stylistic/ts/indent': ['error', 2],
+      '@stylistic/ts/quote-props': ['error', 'as-needed'],
+      '@stylistic/ts/semi': ['warn', 'always'],
+      '@stylistic/ts/quotes': ['warn', 'single'],
+      '@stylistic/ts/quote-props': ['warn', 'as-needed'],
+      '@stylistic/ts/comma-dangle': ['warn', {
+        'arrays': 'always-multiline',
+        'objects': 'always-multiline',
+        'imports': 'always-multiline',
+        'exports': 'always-multiline',
+        'functions': 'never',
+        'enums': 'never',
+        'generics': 'never',
+        'tuples': 'never',
+      }],
+      '@stylistic/ts/object-curly-spacing': ['warn', 'always'],
+      '@stylistic/ts/space-before-function-paren': ['warn', {
+        named: 'never',
+        asyncArrow: 'always',
+        anonymous: 'always',
+      }],
+      '@stylistic/ts/quotes': ['warn', 'single', {
+        avoidEscape: true
+      }],
+      '@stylistic/ts/object-property-newline': 'off'
+    }
+  }
+];
