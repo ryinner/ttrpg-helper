25b9b8d4f146233a139fdd5c7c22f61ed3360a29
ryinner <vinograd18062002@gmail.com>
2024-07-12 14:45:17 +0500

    prisma service

---
+++ apps/api/src/prisma/prisma.service.ts
@@ -0,0 +1,20 @@
+import {
+  Injectable,
+  type OnApplicationShutdown,
+  type OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnApplicationShutdown
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onApplicationShutdown() {
+    await this.$disconnect();
+  }
+}
