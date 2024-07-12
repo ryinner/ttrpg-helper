156cbdcf3638a4bb58e90259a786190ea8839ac3
ryinner <vinograd18062002@gmail.com>
2024-07-12 10:22:20 +0500

    TTRPGS and refactoring

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -2,15 +2,27 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+enum Languages {
+  Russian = 1,
+  English = 2,
+}
+
+enum TTRPGS {
+  VampireTheMasquerade = 1,
+  DungeonsAndDragons = 2,
+}
+
 async function languages(): Promise<void> {
   const languagesCount = await prisma.language.count();
   if (languagesCount === 0) {
     await prisma.language.createMany({
       data: [
         {
+          id: Languages.Russian,
           code: 'ru',
         },
         {
+          id: Languages.English,
           code: 'en',
         },
       ],
@@ -18,8 +30,53 @@ async function languages(): Promise<void> {
   }
 }
 
+async function ttrpgs(): Promise<void> {
+  const systemsCount = await prisma.tTRPG.count();
+  if (systemsCount === 0) {
+    await prisma.tTRPG.create({
+      data: {
+        id: TTRPGS.VampireTheMasquerade,
+        translates: {
+          createMany: {
+            data: [
+              {
+                languageId: Languages.Russian,
+                name: 'Вампиры Маскарад',
+              },
+              {
+                languageId: Languages.English,
+                name: 'Vampire The Masquerade',
+              },
+            ],
+          },
+        },
+      },
+    });
+    await prisma.tTRPG.create({
+      data: {
+        id: TTRPGS.DungeonsAndDragons,
+        translates: {
+          createMany: {
+            data: [
+              {
+                languageId: Languages.Russian,
+                name: 'Подземелья и Драконы',
+              },
+              {
+                languageId: Languages.English,
+                name: 'Dungeons and Dragons',
+              },
+            ],
+          },
+        },
+      },
+    });
+  }
+}
+
 async function main(): Promise<void> {
   await languages();
+  await ttrpgs();
   await Promise.all([]);
 }
 
