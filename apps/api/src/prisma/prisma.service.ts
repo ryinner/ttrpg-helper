277a6c12f37b1c898a65bd03c2678a3c19444c0e
ryinner <vinograd18062002@gmail.com>
2024-07-15 10:20:48 +0500

    remove unneeded

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,20 +1,5 @@
-import {
-  Injectable,
-  type OnApplicationShutdown,
-  type OnModuleInit,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnApplicationShutdown
-{
-  async onModuleInit() {
-    await this.$connect();
-  }
-
-  async onApplicationShutdown() {
-    await this.$disconnect();
-  }
-}
+export class PrismaService extends PrismaClient {}
