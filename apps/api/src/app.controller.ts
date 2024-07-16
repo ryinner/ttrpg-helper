f8459900b6ac69197ce7b69d577d79e42aff7eb3
ryinner <vinograd18062002@gmail.com>
2024-07-16 16:56:47 +0500

    api swagger

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -2,12 +2,22 @@ import { Controller, Get, Res } from '@nestjs/common';
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
 import { AppService } from './app.service';
 import { type Response } from 'express';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'check heals of api',
+    example: {
+      state: 'ok',
+      version: '1.0.0',
+      description: 'app description',
+    },
+  })
   index(@Res() res: Response) {
     return res.json(this.appService.index());
   }
