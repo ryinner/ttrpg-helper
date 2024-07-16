772936dd8390245a93a6491d2ae788922d9a41aa
ryinner <vinograd18062002@gmail.com>
2024-07-16 16:58:56 +0500

    api property swagger

--- apps/api/src/cards/dto/create-card.dto.ts
+++ apps/api/src/cards/dto/create-card.dto.ts
@@ -1,7 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCardDto {
+  @ApiProperty({ required: true })
   name: string;
 
+  @ApiProperty({ required: true })
   description: string;
 
+  @ApiProperty({ required: true })
   languageId: number;
 }
--- apps/api/src/cards/dto/update-card.dto.ts
+++ apps/api/src/cards/dto/update-card.dto.ts
@@ -1,6 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCardDto } from './create-card.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateCardDto extends PartialType(CreateCardDto) {
+  @ApiProperty({ required: true })
   id: number;
 }
