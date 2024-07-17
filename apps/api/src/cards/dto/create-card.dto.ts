8cd16dc6bda5552870948a0257c39cc1a0e67d03
ryinner <vinograd18062002@gmail.com>
2024-07-17 12:30:28 +0500

    class-validator create && update dto

--- apps/api/src/cards/dto/create-card.dto.ts
+++ apps/api/src/cards/dto/create-card.dto.ts
@@ -1,13 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { ICreateCardDto, Languages } from '@repo/api-sdk';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateCardDto implements ICreateCardDto {
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty({ required: true })
   name: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(512)
   @ApiProperty({ required: true })
   description: string;
 
+  @IsInt()
+  @IsOptional()
   @ApiProperty({ required: false })
   languageId: number = Languages.Russian;
 }
--- apps/api/src/cards/dto/update-card.dto.ts
+++ apps/api/src/cards/dto/update-card.dto.ts
@@ -1,11 +1,14 @@
 import { IUpdateCardDto } from '@repo/api-sdk';
 import { CreateCardDto } from './create-card.dto';
 import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class UpdateCardDto
   extends PartialType(CreateCardDto)
   implements IUpdateCardDto
 {
+  @IsInt()
+  @IsNotEmpty()
   @ApiProperty({ required: true })
   id: number;
 }
