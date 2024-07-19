e8cd71a09a4b138cdcaf0920a64f266d7849e4d1
ryinner <vinograd18062002@gmail.com>
2024-07-19 20:48:09 +0500

    simple telegram bot

--- apps/telegram-bot/src/index.ts
+++ apps/telegram-bot/src/index.ts
@@ -1,5 +1,13 @@
 import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.on(message('text'), async (ctx) => {
+  const {
+    user: { username },
+  } = await ctx.getChatMember(ctx.chat.id);
+  await ctx.reply(`Hello ${username}`);
+});
 
 bot.launch();
