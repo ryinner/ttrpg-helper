b0235599b4864e1549cff68fbf7734483dddc8aa
ryinner <vinograd18062002@gmail.com>
2024-07-22 16:32:52 +0500

    collection api

---
+++ packages/api-sdk/src/api/collection.ts
@@ -0,0 +1,19 @@
+import type { ICardEntity } from '../@types/card';
+import type { ICollectionEntity } from '../@types/collection';
+import { BaseApi } from './api';
+
+export class CollectionApi extends BaseApi {
+  private prefix = 'collection';
+
+  public async get(): Promise<ICollectionEntity[]> {
+    return await this.request(this.prefix, { method: 'GET' });
+  }
+
+  public async getOne(id: number): Promise<ICollectionEntity> {
+    return await this.request(`${this.prefix}/${id}`, { method: 'GET' });
+  }
+
+  public async cards(id: number): Promise<ICardEntity[]> {
+    return await this.request(`${this.prefix}/${id}/cards`, { method: 'GET' });
+  }
+}
