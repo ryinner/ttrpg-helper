ed068f8ce72d03ba01e0931ec10774d95d82e584
ryinner <vinograd18062002@gmail.com>
2024-07-30 10:50:24 +0500

    auth sihn-in entity && dto && swagger

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInDto } from './dto/sign-in.dto';
+import { ApiForbiddenResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/sign-in')
+  @ApiOkResponse()
+  @ApiForbiddenResponse()
+  signIn(@Body() signInDto: SignInDto) {
+    console.log(signInDto);
+    return {};
+  }
 }
